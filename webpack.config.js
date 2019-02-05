f4281ceefaacf89c61fefca62c97095e80a8cd24
Brian Casey <Brian.Casey@laurenec.com>
2019-02-05 13:54:22 -0600

    add some stuff

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
     entry: ['babel-polyfill', './src/index.js'],
     output: {
-        path: path.resolve(__dirname, 'public/scripts'),
+        path: path.resolve(__dirname, 'public/dist'),
         filename: 'bundle.js'
     },
     module: {
@@ -21,7 +21,7 @@ module.exports = {
     },
     devServer: {
         contentBase: path.resolve(__dirname, 'public'),
-        publicPath: '/scripts/'
+        publicPath: '/dist/'
     },
     devtool: 'source-map'
 }
