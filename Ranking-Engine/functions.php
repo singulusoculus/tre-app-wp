7bec37cc2287a4992ab164a6ac0b16b7cfc1d0a2
Brian Casey <singulusoculus@gmail.com>
2019-09-24 13:23:56 -0500

    Fix bgg collection bug where it would not return collections with only 1 play. Bumped to version 2.0.9

--- Ranking-Engine/functions.php
+++ Ranking-Engine/functions.php
@@ -21,7 +21,7 @@ function wpse_44020_logout_redirect( $logouturl, $redir )
 add_filter( 'logout_url', 'wpse_44020_logout_redirect', 10, 2 );
 
 function getRankingEngineVersion () {
-  return '2.0.8';
+  return '2.0.9';
 }
 
 //For The Ranking Engine - starting in 1.4
