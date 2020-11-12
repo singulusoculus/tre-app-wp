ebc7ffb76adc0a341ad57e63dfce2cea54ff270b
Brian Casey <singulusoculus@gmail.com>
2020-11-12 09:53:30 -0600

    -> 2.1.13

--- Ranking-Engine/functions.php
+++ Ranking-Engine/functions.php
@@ -23,7 +23,7 @@ function wpse_44020_logout_redirect( $logouturl, $redir )
 add_filter( 'logout_url', 'wpse_44020_logout_redirect', 10, 2 );
 
 function getRankingEngineVersion () {
-  return '2.1.12';
+  return '2.1.13';
 }
 
 //For The Ranking Engine - starting in 1.4
